297060ee5e2ea9c18160f89aa2dfef7e1fe87e0b
ernesthalili <ernesthalili@yahoo.com>
2021-09-21 10:07:54 +0200

    Modifiche ulteriori

--- src/collision_avoidance/src/controller_node.cpp
+++ src/collision_avoidance/src/controller_node.cpp
@@ -4,29 +4,32 @@
 #include <geometry_msgs/Twist.h>
 #include <stdio.h>
 #include <cmath>
+#include <ctime>
 #include "std_msgs/String.h"
 
-#define PARAM_FRONTE 500        // Parametro che definisce il range dei valori da togliere sia da sinistra che da destra e mantenere solo i valori frontali
-#define PARAM_VISUALE 400       // Parametro che definisce il range dei valori da togliere sia da sinistra che da destra e mantenere solo i valori centrali
+#define PARAM_FRONTE 430        // Parametro che definisce il range dei valori da togliere sia da sinistra che da destra e mantenere solo i valori frontali
+#define PARAM_VISUALE 380       // Parametro che definisce il range dei valori da togliere sia da sinistra che da destra e mantenere solo i valori centrali
 #define WARNING_CENTER_PARAM 1  // Definisce la distanza di sicurezza (per valori del laser > 1 , la base mobile non reagisce agli ostacoli)
-#define WARNING_FRONT_PARAM 0.2
+#define WARNING_FRONT_PARAM 0.2 // Distanza per cui il robot si considera in trapola
 #define K_OSTACOLI 0.000001  // 10^(-6)
 #define K_VELOCITA_IMPOSTA 10000 // 10^(4)
-
-bool decisione_da_prendere=true;
-float valore_decisione=0;
-
-bool vel_letto=false;
 
 struct Forza {
 	float x_comp;
 	float y_comp;
 };
 
-float angolo_base_mobile;
-Forza* f_att = (Forza*) malloc( sizeof( Forza ) );
-Forza* f_rep = (Forza*) malloc( sizeof( Forza ) );
-Forza* f_ris = (Forza*) malloc( sizeof( Forza ) );
+bool decisione_da_prendere=true;       // utile per robot in trapola
+float valore_decisione=0;
+
+time_t tempo_ultimo_aggiornamento;       // riferisce al tempo dell'ultimo commando di velocita in input
+
+float angolo_base_mobile;				// angolo che la base mobile ha nel sisema di riferimento della mappa
+
+Forza* f_att; 
+Forza* f_rep;
+Forza* f_ris;
+
 
 
 
@@ -70,7 +73,7 @@ void callback_odom(const nav_msgs::Odometry::ConstPtr& msg)
 void callback_input(const geometry_msgs::Twist::ConstPtr& msg)
 {
 	vel_input = *msg;
-	vel_letto=true;
+	time(&tempo_ultimo_aggiornamento);
 }
 
 //main
@@ -93,6 +96,9 @@ int main(int argc , char* argv [])
 	
 	ros::Subscriber command_sub = n.subscribe("/vel_path_follower",1,callback_input); // legge dal topic /vel_path_follower messaggi del tipo: geometry_msgs/Twist
 	 
+	f_att = (Forza*) malloc( sizeof( Forza ) );
+	f_rep = (Forza*) malloc( sizeof( Forza ) );
+	f_ris = (Forza*) malloc( sizeof( Forza ) );
 	ros::Rate r(1000);						
 	while(ros::ok())
 	{
@@ -100,10 +106,19 @@ int main(int argc , char* argv [])
 		vel_stageros.linear.x=0;											 
 		vel_stageros.angular.z=0;
 		
-		if(vel_letto  == true)  {vel_letto = false;}
-		else {vel_input.linear.x=0 ; vel_input.angular.z=0;}
+		time_t tempo_attuale;            
+		time(&tempo_attuale);
 		
-		
+		double seconds;
+		seconds = difftime(tempo_attuale,tempo_ultimo_aggiornamento);      // differenza in secondi tra il tempo attuale e quello del ultimo aggiornamento della velocita in input
+	
+		//printf("Differenza in secondi : %f\n",seconds);
+		if( seconds > 1.1)
+		{
+			vel_input.linear.x=0;
+			vel_input.angular.z=0;
+			//printf("Tempo anullato!\n");
+		}
 		
 		int len = laser.ranges.size();				           				// misura la lunghezza dell'array che mantieni i valori del laser (1081)
 		int lunghezza_centro=len - 2*PARAM_VISUALE;							// misura la lunghezza dell'array che mantiene solo i valori centrali del laser
@@ -152,41 +167,16 @@ int main(int argc , char* argv [])
 				}
 			}
 			
-			//printf("Distanza piu a destra:%f\n",centro[0]);
-			//printf("Distanza piu a sinistra:%f\n",centro[lunghezza_centro]);
-			//printf("F_x_r:%f  ;  F_y_r:%f\n",f_rep->x_comp,f_rep->y_comp);
-			//printf("F_x_a:%f  ;  F_y_a:%f\n",f_att->x_comp,f_att->y_comp);
 			
-			//Angolo imposto dagli ostacoli frontali
-			/*float angolo_rep=0;
-			if(f_rep->x_comp != 0 || f_rep->y_comp != 0)
-			{
-				angolo_rep = atan(f_rep->y_comp/f_rep->x_comp);
-				//printf("Angolo rep non modificato:%f\n",angolo_rep);
-				if(f_rep->x_comp < 0 )
-				{ 
-					if(f_rep->y_comp > 0 ) angolo_rep = angolo_rep + M_PI;
-					else angolo_rep = angolo_rep - M_PI;
-				}
-			}*/
 			f_att->x_comp = vel_input.linear.x * cos(angolo_base_mobile);	
 			f_att->y_comp = vel_input.linear.x * sin(angolo_base_mobile);
-		
-			// Angolo imposto dalla velocita
-			/*float angolo_att=0;
-			if(f_att->x_comp != 0 || f_att->y_comp != 0 )
-			{
-				 angolo_att = atan(f_att->y_comp/f_att->x_comp);
-				 if(angolo_base_mobile > M_PI/2 ) angolo_att += M_PI;
-				 if(angolo_base_mobile < - M_PI/2 ) angolo_att -= M_PI;
-			}*/
 			
 			//Angolo Giusto
 			float angolo_giusto=angolo_base_mobile;
 			f_ris->x_comp = f_att->x_comp + f_rep->x_comp ;					// Sommo i componenti secondo X e Y delle forze agenti sulla base mobile
 			f_ris->y_comp = f_att->y_comp + f_rep->y_comp ;
 			
-			// Calclo l'angolo che la direzione della forza risultante crea con l'asse delle ascisse 
+			// Calcolo l'angolo che la direzione della forza risultante crea con l'asse delle ascisse 
 			if(f_ris->x_comp != 0  || f_ris->y_comp != 0)
 			{
 				angolo_giusto = atan(f_ris->y_comp / f_ris->x_comp);
@@ -196,31 +186,24 @@ int main(int argc , char* argv [])
 					else angolo_giusto -= M_PI;
 				}
 			}
-			//printf("Angolo giusto:%f\n",angolo_giusto);
-			//printf("Angolo rep: %f\n",angolo_rep);
-			//printf("F_x_a:%f  ;  F_y_a:%f\n",f_rep->x_comp,f_rep->y_comp);
-			//printf("Angolo att: %f\n",angolo_att);
-			//printf("F_x_a:%f  ;  F_y_a:%f\n",f_att->x_comp,f_att->y_comp);
-			//printf("Angolo base mobile: %f\n",angolo_base_mobile);
-			//vel_stageros.linear.x=f_att->x_comp / cos(angolo_giusto);
 			
 			float v_a_imposta = 0;											// La velocita angolare che viene aggiunta a quella del joystick 
 																			// per ridirezzionare la base mobile secondo il verso giusto
 			// Se non mando una velocita lineare V_A_IMPOSTA sara nulla
 			if(vel_input.linear.x != 0) v_a_imposta = K_VELOCITA_IMPOSTA * fmodf(angolo_giusto - angolo_base_mobile  , M_PI);
-			printf("Velocita imposta :%f\n",v_a_imposta);
-			printf("\n");
+			//printf("Velocita imposta :%f\n",v_a_imposta);
+			//printf("\n");
 			vel_stageros.angular.z = vel_input.angular.z + v_a_imposta;  // Velocita angolare da mandare al nodo /stageros
 			
 			float k_l_imposta = 1;                                           // coefficiente che moltiplica la velocita lineare della base mobile ed e' 
-																		    // tanto piccolo quanto piu corta e' la distanza dagli ostacoli
+			          													    // tanto piccolo quanto piu corta e' la distanza dagli ostacoli
 			float valore_min_fronte = min_array(fronte , lunghezza_fronte);			// Uso della funzione aux per calcolare la minima distanza che l'array misura
 			if( valore_min_fronte < WARNING_FRONT_PARAM ) 
 			{
-				k_l_imposta = 0;							// Evita che la base mobile viene sbattuta verso il muro quando viene trovato in trapola
+				k_l_imposta = 0;						                 	// Evita che la base mobile viene sbattuta verso il muro quando viene trovato in trapola
 				if(decisione_da_prendere == true)
 				{
-						printf("Cambio valore_decisione\n");
+						//printf("Cambio valore_decisione\n");
 						decisione_da_prendere=false;
 						if(v_a_imposta > 0 ) valore_decisione=0.5;
 						else valore_decisione=-0.5;
@@ -229,12 +212,12 @@ int main(int argc , char* argv [])
 			}
 			else
 			{
-					printf("Cambio valore_decisione a 0\n");
+					//printf("Cambio valore_decisione a 0\n");
 					decisione_da_prendere=true;
 					valore_decisione=0;
 			}
-			printf("valore k_l_imposta:%f\n",k_l_imposta);
-			printf("valore_decisione : %f\n",valore_decisione);
+			//printf("valore k_l_imposta:%f\n",k_l_imposta);
+			//printf("valore_decisione : %f\n",valore_decisione);
 			float valore_min_centro = min_array(centro,lunghezza_centro);
 			if( valore_min_centro <  WARNING_CENTER_PARAM )
 			{  
@@ -253,11 +236,17 @@ int main(int argc , char* argv [])
 			}
 			
 			vel_pub.publish(vel_stageros);								// publica la velocita
-			
+			//printf("Velocita lineare output : %f \n",vel_stageros.linear.x);
+			//printf("Velocita angolare output : %f \n",vel_stageros.angular.z);
+			//printf("\n");
 		}
 	    ros::spinOnce();
 	    r.sleep();
 	}
+	
+	free(f_att);
+	free(f_rep);
+	free(f_ris);
 	
 	return 0;
 }
